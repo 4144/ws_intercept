befc0e7e5d852414b8bb2fe71653a847734476a9
VariantXYZ <vijayganesan1@yahoo.ca>
2014-08-12 23:53:28 +0000

    Modified header to support both C++ and C plugins

--- source/ws.h
+++ source/ws.h
@@ -4,10 +4,18 @@
 #include <windows.h>
 #include "list.h"
 
-#ifdef EXPORT
-#define LIBAPI __declspec(dllexport)
+#ifdef __cplusplus
+#define EXT extern "C"
 #else
-#define LIBAPI __declspec(dllimport)
+#define EXT
+#endif
+
+#ifdef EXPORT
+#define LIBVAR __declspec(dllexport)
+#define LIBAPI EXT LIBVAR
+#else
+#define LIBVAR __declspec(dllimport)
+#define LIBAPI EXT LIBVAR
 #endif
 
 typedef void (WINAPI *tWS_plugin)(SOCKET&, const char*, int&, int&); //For plugin hooks, returns plugin ID
@@ -24,12 +32,12 @@ struct WS_handler
 	char *comment;
 };
 
-extern "C" LIBAPI DWORD register_handler(tWS_plugin func, WS_HANDLER_TYPE type, char *comment = (char*)""); //0x0 affects all packets
-extern "C" LIBAPI void unregister_handler(DWORD plugin_id, WS_HANDLER_TYPE type);
+LIBAPI DWORD register_handler(tWS_plugin func, WS_HANDLER_TYPE type, char *comment = (char*)""); //0x0 affects all packets
+LIBAPI void unregister_handler(DWORD plugin_id, WS_HANDLER_TYPE type);
 
-LIBAPI list<HMODULE> ws_plugins; //Why is the list of DLLs exposed? So someone can implement a plugin to reload them, of course!
-LIBAPI list<WS_handler> ws_handlers_send; //Contains ordered list of function handlers for send
-LIBAPI list<WS_handler> ws_handlers_recv; //Contains ordered list of function handlers for recv
+LIBVAR list<HMODULE> ws_plugins; //Why is the list of DLLs exposed? So someone can implement a plugin to reload them, of course!
+LIBVAR list<WS_handler> ws_handlers_send; //Contains ordered list of function handlers for send
+LIBVAR list<WS_handler> ws_handlers_recv; //Contains ordered list of function handlers for recv
 
 
 #endif //WS_SEND_H
